9ff54c1acf70e0c13e67d8ab07c8ed3503398bcb
gilvano <g.piontkoski@gmail.com>
2021-07-01 23:54:12 -0300

    Adicionar UsersController

---
+++ src/AdvancedTaskControl.API/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using AdvancedTaskControl.API.Models;
+using AdvancedTaskControl.Data.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AdvancedTaskControl.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly MeuDbContext _context;
+
+        public UsersController(MeuDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> AddUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+    }
+}
