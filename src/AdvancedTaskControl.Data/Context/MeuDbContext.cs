7362e3f4f594fd364aa8a3bff38623daeab4cbac
gilvano <g.piontkoski@gmail.com>
2021-07-01 22:32:48 -0300

    Adicionar DbContext

---
+++ src/AdvancedTaskControl.Data/Context/MeuDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AdvancedTaskControl.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedTaskControl.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions<MeuDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> TodoItems { get; set; }
+    }
+}
