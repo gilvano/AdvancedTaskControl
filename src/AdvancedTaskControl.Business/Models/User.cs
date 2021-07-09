b63795df86548dd77a0a579a7426acaf4c351518
gilvano <g.piontkoski@gmail.com>
2021-07-09 14:40:57 -0300

    Add user role

--- src/AdvancedTaskControl.Business/Models/User.cs
+++ src/AdvancedTaskControl.Business/Models/User.cs
@@ -11,6 +11,7 @@ namespace AdvancedTaskControl.API.Models
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Role { get; set; }
 
 
     }
---
+++ src/AdvancedTaskControl.Data/Migrations/20210709173620_Add user role.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvancedTaskControl.Data.Migrations
+{
+    public partial class Adduserrole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Username",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+        }
+    }
+}
