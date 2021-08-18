49ebf6c4e37ff290c999f0fba5424118920ab9ab
Gilvano Piontkoski <g.piontkoski@gmail.com>
2021-08-18 10:13:53 -0300

    Adicionar campos de data de criação e modificação nas tarefas.

--- src/AdvancedTaskControl.Data/Migrations/20210818130419_Add fields created and modified date 2.Designer.cs
+++ src/AdvancedTaskControl.Data/Migrations/20210818130419_Add fields created and modified date 2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using AdvancedTaskControl.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace AdvancedTaskControl.Data.Migrations
 {
     [DbContext(typeof(MeuDbContext))]
-    partial class MeuDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210818130419_Add fields created and modified date 2")]
+    partial class Addfieldscreatedandmodifieddate2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
