96ee8088c30ea181f6de14a191b1077808056328
Gilvano Piontkoski <g.piontkoski@gmail.com>
2021-08-18 10:18:31 -0300

    Adicionar campos de data de criação e modificação nas categorias.

--- src/AdvancedTaskControl.Business/Models/Category.cs
+++ src/AdvancedTaskControl.Business/Models/Category.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace AdvancedTaskControl.Business.Models
 {
-    public class Category
+    public class Category : EntityBase
     {
         public int Id { get; set; }
         public string CategoryDescription { get; set; }
